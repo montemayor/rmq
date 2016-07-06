fed7fbcde0fa8b7479d9052196ec54a55bf53172
Ryan Leary <ryan@qntfy.io>
2016-07-06 11:45:58 -0400

    Switch RMQ to use only redis-cluster

--- queue.go
+++ queue.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/adjust/uniuri"
-	"gopkg.in/redis.v3"
+	"gopkg.in/redis.v4"
 )
 
 const (
@@ -50,14 +50,14 @@ type redisQueue struct {
 	rejectedKey      string // key to list of rejected deliveries
 	unackedKey       string // key to list of currently consuming deliveries
 	pushKey          string // key to list of pushed deliveries
-	redisClient      *redis.Client
+	redisClient      *redis.ClusterClient
 	deliveryChan     chan Delivery // nil for publish channels, not nil for consuming channels
 	prefetchLimit    int           // max number of prefetched deliveries number of unacked can go up to prefetchLimit + numConsumers
 	pollDuration     time.Duration
 	consumingStopped bool
 }
 
-func newQueue(name, connectionName, queuesKey string, redisClient *redis.Client) *redisQueue {
+func newQueue(name, connectionName, queuesKey string, redisClient *redis.ClusterClient) *redisQueue {
 	consumersKey := strings.Replace(connectionQueueConsumersTemplate, phConnection, connectionName, 1)
 	consumersKey = strings.Replace(consumersKey, phQueue, name, 1)
 
