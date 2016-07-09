f277abcc96e441090b72e3d4ed15eb6ff1e870cd
Ryan Leary <ryan@qntfy.io>
2016-07-08 23:07:55 -0400

    Add hash tags to prevent crossslot error
    Replace square brackets with curly brackets

--- queue.go
+++ queue.go
@@ -14,12 +14,12 @@ const (
 	connectionsKey                   = "rmq::connections"                                           // Set of connection names
 	connectionHeartbeatTemplate      = "rmq::connection::{connection}::heartbeat"                   // expires after {connection} died
 	connectionQueuesTemplate         = "rmq::connection::{connection}::queues"                      // Set of queues consumers of {connection} are consuming
-	connectionQueueConsumersTemplate = "rmq::connection::{connection}::queue::[{queue}]::consumers" // Set of all consumers from {connection} consuming from {queue}
-	connectionQueueUnackedTemplate   = "rmq::connection::{connection}::queue::[{queue}]::unacked"   // List of deliveries consumers of {connection} are currently consuming
+	connectionQueueConsumersTemplate = "rmq::connection::{connection}::queue::{{queue}}::consumers" // Set of all consumers from {connection} consuming from {queue}
+	connectionQueueUnackedTemplate   = "rmq::connection::{connection}::queue::{{queue}}::unacked"   // List of deliveries consumers of {connection} are currently consuming
 
 	queuesKey             = "rmq::queues"                     // Set of all open queues
-	queueReadyTemplate    = "rmq::queue::[{queue}]::ready"    // List of deliveries in that {queue} (right is first and oldest, left is last and youngest)
-	queueRejectedTemplate = "rmq::queue::[{queue}]::rejected" // List of rejected deliveries from that {queue}
+	queueReadyTemplate    = "rmq::queue::{{queue}}::ready"    // List of deliveries in that {queue} (right is first and oldest, left is last and youngest)
+	queueRejectedTemplate = "rmq::queue::{{queue}}::rejected" // List of rejected deliveries from that {queue}
 
 	phConnection = "{connection}" // connection name
 	phQueue      = "{queue}"      // queue name
@@ -342,7 +342,7 @@ func (queue *redisQueue) consumeBatch(batchSize int) bool {
 
 	if err != nil && err != redis.Nil {
 		// TODO: Not sure what to do here just yet
-		debug("Unexpected error occurred")
+		log.Panic("Unexpected error occurred.", err)
 	}
 
 	for _, result := range reqs {
